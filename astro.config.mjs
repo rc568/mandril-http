d167eaca83cdb90d51699ae214e0e5fb3080b091
irojas <isaac568r@gmail.com>
2025-03-17 17:52:58 -0500

    remove node adapter / add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
-import node from '@astrojs/node'
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  adapter: node({
-    mode: 'standalone'
-  })
+
+  adapter: cloudflare()
 })
